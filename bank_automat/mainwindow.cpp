6df743ab1aed1acdd63f454d6e5333bdbe1617a6
Yarviz <juzzzzi@hotmail.com>
2020-11-09 22:08:36 +0200

    little update

--- bank_automat/mainwindow.cpp
+++ bank_automat/mainwindow.cpp
@@ -6,6 +6,26 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    card_string = "Syötä kortti";
+    card_number = 0;
+    card_pin = 0;
+    no_string = true;
+
+    connect(ui->Btn_0, SIGNAL(pressed()), this, SLOT(digitClick()));
+    connect(ui->Btn_1, SIGNAL(pressed()), this, SLOT(digitClick()));
+    connect(ui->Btn_2, SIGNAL(pressed()), this, SLOT(digitClick()));
+    connect(ui->Btn_3, SIGNAL(pressed()), this, SLOT(digitClick()));
+    connect(ui->Btn_4, SIGNAL(pressed()), this, SLOT(digitClick()));
+    connect(ui->Btn_5, SIGNAL(pressed()), this, SLOT(digitClick()));
+    connect(ui->Btn_6, SIGNAL(pressed()), this, SLOT(digitClick()));
+    connect(ui->Btn_7, SIGNAL(pressed()), this, SLOT(digitClick()));
+    connect(ui->Btn_8, SIGNAL(pressed()), this, SLOT(digitClick()));
+    connect(ui->Btn_9, SIGNAL(pressed()), this, SLOT(digitClick()));
+
+    connect(ui->Btn_STOP, SIGNAL(pressed()), this, SLOT(stopClick()));
+
+    ui->LabelCardInput->setText(card_string);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +33,29 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::digitClick()
+{
+    if (card_string.size() == MAX_CARD_SIZE) return;
+
+    QPushButton *clickedButton = qobject_cast<QPushButton *>(sender());
+    QString digit = clickedButton->text();
+
+    if (no_string)
+    {
+        ui->LabelCardInput->setStyleSheet("background-color: white; color: black;");
+        card_string = "";
+        no_string = false;
+    }
+
+    card_string += digit;
+    ui->LabelCardInput->setText(card_string);
+}
+
+void MainWindow::stopClick()
+{
+    ui->LabelCardInput->setStyleSheet("background-color: white; color: gray;");
+    card_string = "Syötä kortti";
+    no_string = true;
+
+    ui->LabelCardInput->setText(card_string);
+}
--- bank_automat/mainwindow.h
+++ bank_automat/mainwindow.h
@@ -1,6 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#define MAX_CARD_SIZE     6
+#define MAX_PIN_SIZE      4
+
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -15,7 +18,15 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void digitClick();
+    void stopClick();
+
 private:
     Ui::MainWindow *ui;
+    QString card_string;
+    qint32  card_number;
+    qint32  card_pin;
+    bool    no_string;
 };
 #endif // MAINWINDOW_H
